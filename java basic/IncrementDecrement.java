a130deaa43e5ef1f95f057675cf4fe1277a1e5c0
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-09-01 07:09:47 +0600

    increment(i++) & Decrement(i--)

---
+++ java basic/IncrementDecrement.java
@@ -0,0 +1,9 @@
+package GeeksforGeeks;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x++;
+        System.out.println(x+" "+y);
+    }
+}
