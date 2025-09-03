b96eb126274e52ea164a2591c07de1ca3dbd506b
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-09-03 08:33:10 +0600

    Learning loops

---
+++ GeeksforGeeks/AlphabetsASCII.java
@@ -0,0 +1,9 @@
+package Loops;
+
+public class AlphabetsASCII {
+    public static void main(String[] args) {
+        for (int i=65; i<=90;i++){
+            System.out.println((char)i+" "+i);
+        }
+    }
+}
