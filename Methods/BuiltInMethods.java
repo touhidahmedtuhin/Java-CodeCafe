223e49656d7588accdc3704da63b5a99ae20a301
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-09-20 07:52:23 +0600

    Built In Method

---
+++ Methods/BuiltInMethods.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class BuiltInMethods {
+    public static void main(String[] args) {
+        System.out.println(Math.max(Math.max(4, 3),5));
+        System.out.println(Math.max(Math.max(4, 3), 9));
+        int max= Math.max(3,4);
+        System.out.println(Math.max(Math.max(10, 3),max));
+
+        System.out.println(Math.max(Math.max(8,6),Math.max(3,4)));
+        System.out.println(Math.pow(3,2));
+    }
+}
